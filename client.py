9e36c23fa49e6f185cb641574b7299141aee1c4a
DylanBonner <dbonner2@huskers.unl.edu>
2025-04-18 00:14:32 -0500

    Generates RSA keys, creates TCP connections
    Still need to implement session keys, authentication, encryption etc.

--- client.py
+++ client.py
@@ -1,1 +1,32 @@
 import socket
+import threading
+import os
+from Crypto.PublicKey import RSA
+
+#save rsa keys to client .pem files
+rsaKey = RSA.generate(2048)
+
+privateKey = rsaKey.export_key()
+with open("client_private.pem", "wb") as file: #save private key to client_private.pem in write binary mode
+  file.write(privateKey)
+
+publicKey = rsaKey.publickey().export_key()
+with open("client_public.pem", "wb") as file:
+  file.write(publicKey)
+
+#create client side TCP socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = ('localhost', 8080)
+clientSocket.connect(address)
+try:
+  while True:
+    #send message to server
+    message = input("Enter message: ")
+    clientSocket.sendall(message.encode('utf-8'))
+    #Receive server response
+    response = clientSocket.recv(1024)
+    print(f"Received from server: "+ str(response.decode('utf-8')))
+except KeyboardInterrupt:
+  print("Client disconnected")
+finally:
+  clientSocket.close()
--- server.py
+++ server.py
@@ -1,1 +1,43 @@
 import socket
+import threading
+import os
+from Crypto.PublicKey import RSA
+
+#save rsa keys to server .pem files
+rsaKey = RSA.generate(2048)
+
+privateKey = rsaKey.export_key()
+with open("server_private.pem", "wb") as file: #save private key to server_private.pem in write binary mode
+  file.write(privateKey)
+
+publicKey = rsaKey.publickey().export_key()
+with open("server_public.pem", "wb") as file:
+  file.write(publicKey)
+
+#create server side TCP socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = ('localhost', 8080)
+serverSocket.bind(address)
+maxQueuedConnections = 5 #number of incoming connections server can wait on before new connections are rejected
+serverSocket.listen(maxQueuedConnections)
+while True:
+  clientSocket, clientAddress = serverSocket.accept()
+  print("New connection from " + str(clientAddress) + " established.")
+  try:
+    while True: #run until client disconnects or sends 'quit'
+      #Receive client message
+      message = clientSocket.recv(1024)
+      if not message or message.decode('utf-8').lower() == "quit": #if client disconnects or sends 'quit'
+        break
+      print(f"Received from client: " + str(message.decode('utf-8')))
+      #send response to client
+      response = "Message received"
+      clientSocket.sendall(response.encode('utf-8'))
+  except ConnectionResetError:
+    print("Client disconnected")
+  finally:
+    clientSocket.close()
+    print("Connection closed")
+
+
+
