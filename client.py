33d28cbad77e5f206c9cde3166ff172a6a734441
Trentyn Beekman <tbeekman2@huskers.unl.edu>
2025-04-18 17:14:20 -0500

    Added RSA-based message encryption and decryption that will later be used for the hybrid RSA and AES encryption

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import os
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 #save rsa keys to client .pem files
 rsaKey = RSA.generate(2048)
@@ -14,6 +15,12 @@ publicKey = rsaKey.publickey().export_key()
 with open("client_public.pem", "wb") as file:
   file.write(publicKey)
 
+#load public key of server
+with open("server_public.pem", "rb") as file:
+  server_public_key = RSA.import_key(file.read())
+  
+rsa_cipher = PKCS1_OAEP.new(server_public_key)
+
 #create client side TCP socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = ('localhost', 8080)
@@ -22,7 +29,8 @@ try:
   while True:
     #send message to server
     message = input("Enter message: ")
-    clientSocket.sendall(message.encode('utf-8'))
+    encrypted_message = rsa_cipher.encrypt(message.encode('utf-8'))
+    clientSocket.sendall(encrypted_message)
     #Receive server response
     response = clientSocket.recv(1024)
     print(f"Received from server: "+ str(response.decode('utf-8')))
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import os
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 #save rsa keys to server .pem files
 rsaKey = RSA.generate(2048)
@@ -13,6 +14,12 @@ with open("server_private.pem", "wb") as file: #save private key to server_priva
 publicKey = rsaKey.publickey().export_key()
 with open("server_public.pem", "wb") as file:
   file.write(publicKey)
+
+#load private key of server
+with open("server_private.pem", "rb") as file:
+  server_private_key = RSA.import_key(file.read())
+
+rsa_cipher = PKCS1_OAEP.new(server_private_key)
 
 #create server side TCP socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,10 +33,14 @@ while True:
   try:
     while True: #run until client disconnects or sends 'quit'
       #Receive client message
-      message = clientSocket.recv(1024)
-      if not message or message.decode('utf-8').lower() == "quit": #if client disconnects or sends 'quit'
+      encrypted_message = clientSocket.recv(1024)
+      if not encrypted_message or encrypted_message.decode('utf-8').lower() == "quit": #if client disconnects or sends 'quit'
         break
-      print(f"Received from client: " + str(message.decode('utf-8')))
+      try:
+        decrypted_message = rsa_cipher.decrypt(encrypted_message).decode('utf-8')
+      except ValueError:
+        decrypted_message = "[Decryption Failed]"
+      print(f"Received from client: {decrypted_message}")
       #send response to client
       response = "Message received"
       clientSocket.sendall(response.encode('utf-8'))
